d02719f21d60e3d29ff5c1cb69979fc807a96d77
luis19gc <alfredo19gc@gmail.com>
2023-05-19 12:45:26 +0200

    first commit

---
+++ src/main/java/com/example/demo/EmpleadosApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleadosApplication.class, args);
+		System.out.println("Hola");
+	}
+
+}
